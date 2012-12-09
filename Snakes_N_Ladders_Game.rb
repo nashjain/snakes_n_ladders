eede0a7d26d69bf62e010dfc5ae48fd334bdae44
Naresh Jain <nashjain@gmail.com>
2012-12-09 10:51:51 +0530

    minor update

--- Snakes_N_Ladders_Game.rb
+++ Snakes_N_Ladders_Game.rb
@@ -1,4 +1,20 @@
+#!/usr/bin/env ruby
+
 class Snakes_N_Ladders_Game
+  def initialize(players, mode=:Easy, board_size=100)
+    @players = players
+    @board_size = board_size
+    game_modes = Hash.new(5).merge({:Easy=>5, :Medium=>10, :Hard=>20})
+    @snakes_ladders = random_board(game_modes[mode], board_size)
+  end
+  
+  def start
+     players_starting_positions = @players.map{0}
+     starting_player = rand(@players.length)
+     winner = move(players_starting_positions, starting_player)
+     puts "#{@players[winner]} won!"
+  end
+  
   def move(players, player_turn)
     new_position = players[player_turn] + throw_dice
     new_position = @snakes_ladders[new_position] if @snakes_ladders.has_key?(new_position)
@@ -7,6 +23,8 @@ class Snakes_N_Ladders_Game
     next_player = (player_turn + 1) % players.length
     move(players, next_player)
   end
+  
+  private
 
   def throw_dice
     1 + rand(6)
@@ -29,20 +47,6 @@ class Snakes_N_Ladders_Game
   
   def random_cell_value(board_size)
     1 + rand(board_size - 1)
-  end
-
-  def initialize(players, mode=:Easy, board_size=100)
-    @players = players
-    @board_size = board_size
-    game_modes = Hash.new(5).merge({:Easy=>5, :Medium=>10, :Hard=>20})
-    @snakes_ladders = random_board(game_modes[mode], board_size)
-  end
-  
-  def start
-     players_starting_positions = @players.map{0}
-     starting_player = rand(@players.length)
-     winner = move(players_starting_positions, starting_player)
-     puts "#{@players[winner]} won!"
   end
 end
 
